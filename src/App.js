acec5ba8e35cdfc74929c992d686098eec2910b9
samyam81 <samyam081@gmail.com>
2024-10-04 10:42:37 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App=()=>{
+    const[formData,setformData]=useState({
+        email:'',
+        password:''
+    });
+
+    const[error,seterror]=useState({});
+    const[IsSubmiited,setIsSubmitted]=useState(false);
+
+      const validate = () => {
+        const newErrors = {};
+
+        if (!formData.email) {
+          newErrors.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+          newErrors.email = "Email is invalid";
+        }
+
+        if (!formData.password) {
+          newErrors.password = "Password is required";
+        } else if (formData.password.length < 6) {
+          newErrors.password = "Password must be at least 6 characters long";
+        }
+
+        return newErrors;
+      };
+
+         const handleChange = (e) => {
+           // Do not update form data until "Enter" is pressed
+           const { name, value } = e.target;
+           setformData({
+             ...formData,
+             [name]: value,
+           });
+         };
+
+      const handleSubmit=(e)=>{
+        const validateErrors=validate();
+        seterror(validateErrors);
+        if(Object.keys(validateErrors).length===0){
+            setIsSubmitted(true);
+        }
+      };
+
+    return (
+      <div className="Form">
+        <h2> Sign In</h2>
+        {IsSubmiited ? (
+          <div className="success-message">Form Submitted Successfully!!</div>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <div className="Email">
+              <label> Email: </label>
+              <input
+                type="text"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              {error.email && <p style={{ color: "red" }}>{error.email}</p>}
+            </div>
+            <div className="Password">
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+              {error.password && (
+                <p style={{ color: "red" }}>{error.password}</p>
+              )}
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </div>
+    );
+};
 
 export default App;
